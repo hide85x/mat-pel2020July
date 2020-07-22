60a8846580800a4c65fb61850af9524161323fac
hide <hide85x@gmail.com>
2020-07-22 13:45:43 +0200

    added animations on hoverOn elemnts of footer

--- docs/index.js
+++ docs/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61821" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61893" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
